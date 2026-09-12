c655fd38672a9a10a585b6231fd004f70553a70d
dhal0077 <dhal0077@gmail.com>
2026-09-12 20:54:38 +0900

    Refresh app cache for daily lock update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kids-sleep-rotation-v3';
+const CACHE='kids-sleep-rotation-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
